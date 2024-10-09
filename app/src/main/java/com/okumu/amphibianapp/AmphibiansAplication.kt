291b3df1fa43b5dc714bf31660ff32b7268cd2b4
Fleur41 <Muriithikings456@gmail.com>
2024-10-09 16:11:05 +0300

    Added on AmphibiansRepository.kt, AppContainer.kt, Amphibian

--- app/src/main/java/com/okumu/amphibianapp/AmphibiansAplication.kt
+++ app/src/main/java/com/okumu/amphibianapp/AmphibiansAplication.kt
@@ -1,4 +1,14 @@
 package com.okumu.amphibianapp
 
-class AmphibiansAplication {
+import android.app.Application
+import com.okumu.amphibianapp.data.AppContainer
+import com.example.amphibians.data.DefaultAppContainer
+
+
+class AmphibiansAplication: Application() {
+    lateinit var container: AppContainer
+    override fun onCreate() {
+        super.onCreate()
+        container = DefaultAppConatiner()
+    }
 }
--- app/src/main/java/com/okumu/amphibianapp/data/AmphibiansRepository.kt
+++ app/src/main/java/com/okumu/amphibianapp/data/AmphibiansRepository.kt
@@ -1,4 +1,12 @@
 package com.okumu.amphibianapp.data
 
-class AmphibiansRepository {
+import com.okumu.amphibianapp.model.Amphibian
+import com.okumu.amphibianapp.network.AmphibiansApiService
+
+interface AmphibiansRepository{
+    suspend fun getAmphibians(): List<Amphibian>
+}
+class DefaultAmphibiansRepository(private val amphibiansApiService: AmphibiansApiService): AmphibiansRepository {
+    override suspend fun getAmphibians(): List<Amphibian> = amphibiansApiService.getAmphibians()
+
 }
